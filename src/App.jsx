2999c9190203ce8707d41bdb4320f2d9b1eddcab
HoangLeCoder <hle070302@gmail.com>
2023-06-21 14:48:29 +0700

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from "react-router-dom"
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarCanvas } from './components'
+
+const App = () => {
+  return (
+    <div>3D Developer Portfolio</div>
+  )
+}
+
+export default App
